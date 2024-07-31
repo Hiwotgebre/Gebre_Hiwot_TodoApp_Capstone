d023f82d1b86eb8dfc7978f0ad6de5baeea5a40c
Hiwotgebre <hiwotg173@gmail.com>
2024-07-30 21:54:38 -0400

    modifying task.js file

--- src/models/task.js
+++ src/models/task.js
@@ -3,14 +3,13 @@ const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  description: String,
-  dueDate: Date,
-  status: String,
-  priority: String,
-}, {
-  timestamps: true,
+  description: { type: String},
+  dueDate: { type: Date, required: true},
+  status: {type: String, enum: ['pending', 'in-progress', 'completed'], default: 'pending' },
+  priority: { type: String, enum: ['low', 'medium', 'high'], default: 'low' }
+ 
 });
 
 const Task = mongoose.model('Task', taskSchema);
 
-module.export =  Task;
+module.exports =  Task;
